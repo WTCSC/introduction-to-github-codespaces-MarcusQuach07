9803150b711d0541fa56ddc33c1d5295656f4d58
Marcus <marcusquach07@gmail.com>
2024-09-16 20:25:23 +0000

    Prints

--- test_hello.py
+++ test_hello.py
@@ -4,3 +4,5 @@ import pytest
 def test_no_arguments():
     result = subprocess.run(['python3', 'hello.py'], stdout=subprocess.PIPE)
     assert result.stdout == b'Hello, GitHub Codespaces!\n'
+    # Prints message
+    print('Hello, GitHub Codespaces!')
